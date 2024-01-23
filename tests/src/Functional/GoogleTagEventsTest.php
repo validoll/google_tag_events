d3a96784b237e0d285000a9058dc953353149821
Vyacheslav Malchik <for@validoll.ru>
2024-01-23 12:28:41 +0500

    Issue #3415521: Can't push the same event twice

---
+++ tests/src/Functional/GoogleTagEventsTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Drupal\Tests\google_tag_events\Functional;
+
+/**
+ * Tests GTM Events.
+ *
+ * @group google_tag_events
+ */
+class GoogleTagEventsTest extends GoogleTagEventsTestsBase {
+
+  /**
+   * Modules to install.
+   *
+   * @var array
+   */
+  protected static $modules = [
+    'google_tag',
+    'google_tag_events',
+    'gtm_events_test',
+  ];
+
+  /**
+   * Test 404 page.
+   */
+  public function testTheSameEventMultipleTimes() {
+    $this->drupalGet('<front>');
+    $this->assertSession()->elementContains('css', 'script[data-selector="google_tag_events"]', '"gtm_events_test_front":{"event":"event_1"}');
+    $this->assertSession()->elementContains('css', 'script[data-selector="google_tag_events"]', '"gtm_events_test_front_1":{"event":"event_2"}');
+  }
+
+}
